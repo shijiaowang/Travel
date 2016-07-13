72560e63a2f9e6872252d0d2a061540ad4671810
wangyang <1440214507@qq.com>
2016-07-13 12:32:45 +0800

    个人中心

---
+++ MyApplication/TravelOfYunNan/src/main/java/com/example/administrator/travel/ui/activity/OtherUserCenterActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.travel.ui.activity;
+
+/**
+ * Created by Administrator on 2016/7/13 0013.
+ */
+public class OtherUserCenterActivity extends BaseActivity {
+    @Override
+    protected int initLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- MyApplication/app/src/main/java/yunshuo/yneb/com/yunnantravel/MainActivity.java
+++ MyApplication/app/src/main/java/yunshuo/yneb/com/yunnantravel/MainActivity.java
@@ -1,76 +1,77 @@
 package yunshuo.yneb.com.yunnantravel;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<LLTest> list =new ArrayList();
+    private ImageView iv_url;
+    private ImageLoader instance;
+    private TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView viewById = (ListView) findViewById(R.id.lv);
-        viewById.setAdapter(new SingleAdapter());
+        instance = ImageLoader.getInstance();
+        instance.init(ImageLoaderConfiguration.createDefault(MainActivity.this));
+        iv_url = (ImageView) findViewById(R.id.iv_url);
+        tv_text = (TextView) findViewById(R.id.tv_text);
+
 
     }
-    class SingleAdapter extends BaseAdapter{
-      List<SingleView> singleViews=new ArrayList<>();
-        @Override
-        public int getCount() {
-            return 10;
-        }
 
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final SingleHolder holder;
-           if (convertView==null){
-               holder=new SingleHolder();
-               convertView=View.inflate(MainActivity.this, R.layout.click, null);
-               holder.singleView= (SingleView) convertView.findViewById(R.id.single_view);
-               convertView.setTag(holder);
-           }else {
-               holder= (SingleHolder) convertView.getTag();
-           }
-            holder.singleView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (singleViews.size() > 0) {
-                        singleViews.get(0).setTitle("取消");
-                        singleViews.remove(0);
-                    }
-                    holder.singleView.setTitle("选中");
-                    singleViews.add(holder.singleView);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String url="http://b.hiphotos.baidu.com/zhidao/pic/item/a6efce1b9d16fdfa9e17a9a1b08f8c5494ee7b01.jpg";
+        instance.displayImage(url,iv_url, DisplayImageOptions.createSimple(), new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                super.onLoadingStarted(imageUri, view);
+                tv_text.setVisibility(View.VISIBLE);
+            }
 
 
-                }
-            });
-            return convertView;
-        }
-        private void add(SingleView singleView){
-            singleViews.add(singleView);
-        }
-    }
-    static class SingleHolder{
-        SingleView singleView;
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                super.onLoadingComplete(imageUri, view, loadedImage);
+                tv_text.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                super.onLoadingFailed(imageUri, view, failReason);
+                tv_text.setVisibility(View.GONE);
+            }
+        }, new ImageLoadingProgressListener() {
+            @Override
+            public void onProgressUpdate(String s, View view, int i, int i1) {
+                tv_text.setText(i/i1+"%");
+            }
+        });
     }
 }
