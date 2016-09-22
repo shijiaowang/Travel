30e570ed8be551fc23a8ef7b22929e535b2a2807
wangyang <1440214507@qq.com>
2016-09-22 18:36:51 +0800

    集成头像裁剪框架

--- MyApplication/TravelOfYunNan/src/main/java/com/example/administrator/travel/ui/activity/BaseActivity.java
+++ MyApplication/TravelOfYunNan/src/main/java/com/example/administrator/travel/ui/activity/BaseActivity.java
@@ -1,15 +1,25 @@
 package com.example.administrator.travel.ui.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.Html;
 
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -19,12 +29,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 
 import com.example.administrator.travel.R;
 import com.example.administrator.travel.utils.LogUtils;
 
 import org.greenrobot.eventbus.EventBus;
+import org.xutils.common.util.DensityUtil;
 import org.xutils.x;
 
 import java.util.List;
@@ -40,7 +52,10 @@ public abstract class BaseActivity extends FragmentActivity {
     public static final int TYPE_DELETE = 4;//删除
     public static final  int REQ_CODE=0;
     public static final  int RESULT_CODE=1;
+    protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101;
+    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102;
 
+    private AlertDialog mAlertDialog;
     public static final int TRAFFIC_TYPE =0;//交通方式
     public static final int SEX_TYPE=1;//性别赛选
     public static final int AUTH_TYPE=2;//认证筛选
@@ -106,6 +121,9 @@ public abstract class BaseActivity extends FragmentActivity {
     @Override
     protected void onStop() {
         super.onStop();
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            mAlertDialog.dismiss();
+        }
     }
 
     @Override
@@ -213,4 +231,55 @@ public abstract class BaseActivity extends FragmentActivity {
         }
         return false;
     }
+
+
+
+
+
+
+    /**
+     * Requests given permission.
+     * If the permission has been denied previously, a Dialog will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    protected void requestPermission(final String permission, String rationale, final int requestCode) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            showAlertDialog(getString(R.string.permission_title_rationale), rationale,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(BaseActivity.this,
+                                    new String[]{permission}, requestCode);
+                        }
+                    }, getString(R.string.label_ok), null, getString(R.string.label_cancel));
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+        }
+    }
+
+    /**
+     * This method shows dialog with given title & message.
+     * Also there is an option to pass onClickListener for positive & negative button.
+     *
+     * @param title                         - dialog title
+     * @param message                       - dialog message
+     * @param onPositiveButtonClickListener - listener for positive button
+     * @param positiveText                  - positive button text
+     * @param onNegativeButtonClickListener - listener for negative button
+     * @param negativeText                  - negative button text
+     */
+    protected void showAlertDialog(@Nullable String title, @Nullable String message,
+                                   @Nullable DialogInterface.OnClickListener onPositiveButtonClickListener,
+                                   @NonNull String positiveText,
+                                   @Nullable DialogInterface.OnClickListener onNegativeButtonClickListener,
+                                   @NonNull String negativeText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton(positiveText, onPositiveButtonClickListener);
+        builder.setNegativeButton(negativeText, onNegativeButtonClickListener);
+        mAlertDialog = builder.show();
+    }
+
+
 }
--- MyApplication/TravelOfYunNan/src/main/java/com/example/administrator/travel/ui/appoint/travelplan/TravelsPlanActivity.java
+++ MyApplication/TravelOfYunNan/src/main/java/com/example/administrator/travel/ui/appoint/travelplan/TravelsPlanActivity.java
@@ -1,21 +1,37 @@
 package com.example.administrator.travel.ui.appoint.travelplan;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bigkoo.pickerview.TimePickerView;
 import com.example.administrator.travel.R;
-import com.example.administrator.travel.TravelsApplication;
 import com.example.administrator.travel.global.GlobalValue;
 import com.example.administrator.travel.global.IVariable;
 import com.example.administrator.travel.ui.activity.BarBaseActivity;
-import com.example.administrator.travel.ui.appoint.cutpicture.CutPictureActivity;
 import com.example.administrator.travel.ui.appoint.personnelequipment.PersonnelEquipmentActivity;
 import com.example.administrator.travel.ui.appoint.lineplan.LineBean;
 import com.example.administrator.travel.ui.appoint.lineplan.LinePlanActivity;
@@ -23,16 +39,28 @@ import com.example.administrator.travel.ui.appoint.popwindow.AppointSpinnerPop;
 import com.example.administrator.travel.ui.appoint.popwindow.SpinnerBean;
 import com.example.administrator.travel.ui.view.GradientTextView;
 import com.example.administrator.travel.utils.ActivityUtils;
+import com.example.administrator.travel.utils.BitmapUtils;
 import com.example.administrator.travel.utils.CalendarUtils;
 import com.example.administrator.travel.utils.GlobalUtils;
+import com.example.administrator.travel.utils.IOUtils;
 import com.example.administrator.travel.utils.JsonUtils;
 import com.example.administrator.travel.utils.LogUtils;
+import com.example.administrator.travel.utils.StringUtils;
 import com.example.administrator.travel.utils.ToastUtils;
+import com.yalantis.ucrop.UCrop;
+import com.yalantis.ucrop.model.AspectRatio;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.json.JSONObject;
+import org.xutils.common.util.DensityUtil;
 import org.xutils.view.annotation.ViewInject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -82,6 +110,12 @@ public class TravelsPlanActivity extends BarBaseActivity implements View.OnClick
     private RelativeLayout mRlTraffic;
     @ViewInject(R.id.tv_icon)
     private TextView mTvIcon;
+    @ViewInject(R.id.rl_icon)
+    private RelativeLayout mRlIcon;
+    @ViewInject(R.id.iv_icon)
+    private ImageView mIvIcon;
+    @ViewInject(R.id.tv_delete)
+    private TextView mTvDelete;
     private TextView mTvRightNext;
     private TimePickerView pvTime;
     private Date startDate = new Date();
@@ -92,6 +126,9 @@ public class TravelsPlanActivity extends BarBaseActivity implements View.OnClick
     private Date endLine;
     private Date startLine;
     private List<SpinnerBean> traffics;
+    private static final int REQUEST_SELECT_PICTURE = 0x01;
+    private static final String IMAGE_NAME = "CropImage";
+    private String filename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -131,12 +168,7 @@ public class TravelsPlanActivity extends BarBaseActivity implements View.OnClick
         mRlEndMorning.setOnClickListener(this);
         mRlStartNight.setOnClickListener(this);
         mRlEndNight.setOnClickListener(this);
-        mTvIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(TravelsPlanActivity.this, CutPictureActivity.class));
-            }
-        });
+        mTvIcon.setOnClickListener(this);
     }
 
 
@@ -194,6 +226,9 @@ public class TravelsPlanActivity extends BarBaseActivity implements View.OnClick
             case R.id.rl_traffic:
                 AppointSpinnerPop.showSpinnerPop(this,mRlTraffic,traffics);
                break;
+            case R.id.tv_icon:
+                showPictureCutPop(this,mBtNext);
+                break;
         }
     }
 
@@ -371,5 +406,251 @@ public class TravelsPlanActivity extends BarBaseActivity implements View.OnClick
         }
         return true;
     }
+    public  void showPictureCutPop(final Activity activity, View view) {
+        // 获取弹出视图对象
+        View viewPopup = View.inflate(activity, R.layout.pop_cut_icon, null);
+        // 创建 弹出窗口
+        final PopupWindow window = new PopupWindow(viewPopup, DensityUtil.getScreenWidth(), DensityUtil.dip2px(151));
+        final WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+        viewPopup.findViewById(R.id.tv_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        viewPopup.findViewById(R.id.tv_album).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickFromGallery();
+            }
+        });
+        viewPopup.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                window.dismiss();
+            }
+        });
+        lp.alpha = 0.7f;
+        activity.getWindow().setAttributes(lp);
+        window.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                lp.alpha = 1f;
+                activity.getWindow().setAttributes(lp);
+            }
+        });
+        // 响应 视图外的地方 点击关闭当前
+        window.setOutsideTouchable(true);
+        // 响应返回键的关闭
+        window.setFocusable(true);
+        window.setBackgroundDrawable(new ColorDrawable(activity.getResources().getColor(android.R.color.transparent)));
+
+        // 获取控件的坐标 x y
+        window.showAtLocation(view, Gravity.TOP,0,DensityUtil.getScreenHeight()-DensityUtil.dip2px(151));
+    }
+    private void pickFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    getString(R.string.permission_read_storage_rationale),
+                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
+        } else {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(Intent.createChooser(intent, getString(R.string.label_select_picture)), REQUEST_SELECT_PICTURE);
+        }
+    }
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickFromGallery();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_SELECT_PICTURE) {
+                final Uri selectedUri = data.getData();
+                if (selectedUri != null) {
+                    startCropActivity(data.getData());
+                } else {
+                    Toast.makeText(TravelsPlanActivity.this, R.string.toast_cannot_retrieve_selected_image, Toast.LENGTH_SHORT).show();
+                }
+            } else if (requestCode == UCrop.REQUEST_CROP) {
+                handleCropResult(data);
+            }
+        }
+        if (resultCode == UCrop.RESULT_ERROR) {
+            handleCropError(data);
+        }
+    }
+    private void handleCropError(@NonNull Intent result) {
+        final Throwable cropError = UCrop.getError(result);
+        if (cropError != null) {
+            Toast.makeText(TravelsPlanActivity.this, cropError.getMessage(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(TravelsPlanActivity.this, R.string.toast_unexpected_error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleCropResult(@NonNull Intent result) {
+        final Uri resultUri = UCrop.getOutput(result);
+        if (resultUri != null) {
+            //ResultActivity.startWithUri(TravelsPlanActivity.this, resultUri);
+            mRlIcon.setVisibility(View.VISIBLE);
+            mTvIcon.setVisibility(View.GONE);
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapUtils.getBitmapFormUri(this,resultUri);
+                //将图片保存在本地
+                mIvIcon.setImageBitmap(bitmap);
+
+                mTvDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mRlIcon.setVisibility(View.GONE);
+                        mTvIcon.setVisibility(View.VISIBLE);
+                    }
+                });
+                saveCroppedImage(bitmap);//保存图片到本地
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                ToastUtils.showToast("未解析到图片");
+            }
+
+
+        } else {
+            Toast.makeText(TravelsPlanActivity.this, R.string.toast_cannot_retrieve_selected_image, Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void saveCroppedImage(Bitmap b) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    getString(R.string.permission_write_storage_rationale),
+                    REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
+        } else {
+            if (b != null) {
+                try {
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        String downloadsDirectoryPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                        String saveName=System.currentTimeMillis()+"icon.jpg";
+                        saveMyBitmap(b,downloadsDirectoryPath,saveName);
+
+                    }else{
+                        ToastUtils.showToast("SD卡未挂载！");
+                        filename=null;
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(TravelsPlanActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(TravelsPlanActivity.this, getString(R.string.toast_unexpected_error), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public boolean saveMyBitmap(Bitmap bmp,String path, String bitName) throws IOException {
+        File f = new File(path + bitName);
+        boolean flag = false;
+        FileOutputStream fOut = null;
+        try {
+            fOut = new FileOutputStream(f);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            flag = true;
+            filename=path+bitName;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (fOut!=null) {
+                fOut.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        IOUtils.close(fOut);
+        return flag;
+    }
+    private void startCropActivity(@NonNull Uri uri) {
+        String destinationFileName = IMAGE_NAME+".jpg";
+        UCrop uCrop = UCrop.of(uri, Uri.fromFile(new File(getCacheDir(), destinationFileName)));
+        uCrop = advancedConfig(uCrop);
+        uCrop.start(TravelsPlanActivity.this);
+    }
+    /**
+     * Sometimes you want to adjust more options, it's done via {@link com.yalantis.ucrop.UCrop.Options} class.
+     *
+     * @param uCrop - ucrop builder instance
+     * @return - ucrop builder instance
+     */
+    private UCrop advancedConfig(@NonNull UCrop uCrop) {
+        UCrop.Options options = new UCrop.Options();
+        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+        options.useSourceImageAspectRatio();
+        options.setShowCropGrid(false);
+        options.setShowCropFrame(false);
+        options.setActiveWidgetColor(getResources().getColor(R.color.otherTitleBg));
+        options.setToolbarColor(getResources().getColor(R.color.otherTitleBg));
+        options.setStatusBarColor(getResources().getColor(R.color.otherTitleBg));
+         options.setAspectRatioOptions(0,new AspectRatio("1",1,1));
+
+        /*
+        If you want to configure how gestures work for all UCropActivity tabs
+
+        options.setAllowedGestures(UCropActivity.SCALE, UCropActivity.ROTATE, UCropActivity.ALL);
+        * */
+
+        /*
+        This sets max size for bitmap that will be decoded from source Uri.
+        More size - more memory allocation, default implementation uses screen diagonal.
+
+        options.setMaxBitmapSize(640);
+        * */
+
+
+       /*
+
+        Tune everything (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+
+        options.setMaxScaleMultiplier(5);
+        options.setImageToCropBoundsAnimDuration(666);
+        options.setDimmedLayerColor(Color.CYAN);
+        options.setCircleDimmedLayer(true);
+        options.setShowCropFrame(false);
+        options.setCropGridStrokeWidth(20);
+        options.setCropGridColor(Color.GREEN);
+        options.setCropGridColumnCount(2);
+        options.setCropGridRowCount(1);
+
+        // Color palette
+        options.setToolbarColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setStatusBarColor(ContextCompat.getColor(this, R.color.your_color_res));
+        options.setActiveWidgetColor(ContextCompat.getColor(this, R.color.your_color_res));
+		options.setToolbarWidgetColor(ContextCompat.getColor(this, R.color.your_color_res));
+
+		// Aspect ratio options
+        options.setAspectRatioOptions(1,
+            new AspectRatio("WOW", 1, 2),
+            new AspectRatio("MUCH", 3, 4),
+            new AspectRatio("RATIO", CropImageView.DEFAULT_ASPECT_RATIO, CropImageView.DEFAULT_ASPECT_RATIO),
+            new AspectRatio("SO", 16, 9),
+            new AspectRatio("ASPECT", 1, 1));
+
+       */
+
+        return uCrop.withOptions(options);
+    }
 }
